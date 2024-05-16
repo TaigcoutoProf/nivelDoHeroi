9cf123fb96ef5e41432ffc5455ed4e2ca0979ff1
TaigcoutoProf <taigcouto.prof@gmail.com>
2024-05-16 13:21:22 -0300

    first commit: desafio01

---
+++ nivelDoHeroi.js
@@ -0,0 +1,28 @@
+let nome = "Jorge";
+let nivel = 5000;
+
+if(nivel<1000){
+    console.log(`O herói de nome ${nome} está no nível Ferro`);
+}
+else if(nivel>=1001 && nivel<2000){
+    console.log(`O herói de nome ${nome} está no nível Bronze`);
+}
+else if(nivel>=2001 && nivel<3000){
+    console.log(`O herói de nome ${nome} está no nível Prata`);
+}
+else if(nivel>=3001 && nivel<4000){
+    console.log(`O herói de nome ${nome} está no nível Ouro`);
+}
+else if(nivel>=4001 && nivel<5000){
+    console.log(`O herói de nome ${nome} está no nível Platina`);
+}
+else if(nivel>=5001 && nivel<6000){
+    console.log(`O herói de nome ${nome} está no nível Ascendente`);
+}
+else if(nivel>=6001 && nivel<7000){
+    console.log(`O herói de nome ${nome} está no nível Imortal`);
+}
+else if(nivel>=7001 && nivel<8000){
+    console.log(`O herói de nome ${nome} está no nível Radiante`);
+}
+
